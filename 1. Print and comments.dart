b6505a4895cdbdbd2a8468a49665c0d5965fe697
Nana-Manuel <emmanuelagbosu29@gmail.com>
2023-09-04 22:58:35 +0000

    First commit

---
+++ 1. Print and comments.dart
@@ -0,0 +1,9 @@
+void main()
+{
+  print("Hello World");  //printing of strings
+  print("This is my frist dart application");
+
+  print(4 + 5); //printing numbers 
+
+  print(false);  //printing booleans 
+}
---
+++ 2. Data Types and Varriables.dart
@@ -0,0 +1,46 @@
+// DATA TYPES
+// integers
+int age = 10;
+var year = 2023;
+
+// foat
+double height = 20.4;
+var m_height = 34.56;
+
+// String 
+var name = "Henry";
+String student_Name = "Peter";
+
+//Boolean
+bool isAlive = true;
+var isDead = false;
+
+// Lists
+// Maps
+// Runes
+// Symbols
+
+
+// The initial values of all varriables is null
+// All data type in dart are Objects
+void main() {
+  print(isAlive);
+
+  //string interpolation
+  String name = "Manuel";
+  String message = "My name is $name"; // we can also use + but its not advisable to do that in dart
+  print(message);
+
+  print("Number of characters in Name: "+ name.length.toString());
+  // OR
+  print("Number of characters in Name: ${name.length}"); // Best practice
+
+  int l = 20;
+  int b = 10;
+  print("Sum of $l and $b is ${l + b}");
+  print("The area of rectangle with lenht $l anf breath $b is ${b * l}");
+}
+
+
+
+
