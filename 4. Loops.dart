0216a8ccf03031125ea27965dc5f14d7cb040e87
Nana-Manuel <emmanuelagbosu29@gmail.com>
2023-09-07 23:04:10 +0000

    added what i studdied on loops

---
+++ 4. Loops.dart
@@ -0,0 +1,19 @@
+//while loop
+var i = 0;
+while (i < 4)
+{
+  print("This is a while loop");
+  i++;
+}
+
+// Do While Loop
+int z = 0;
+do {
+  print("This is a do while loop");
+  i++;
+} while(z < 4);
+
+//For Loop
+for (var x = 0; x < 10; x++){
+  print("This is a for loop");
+}
