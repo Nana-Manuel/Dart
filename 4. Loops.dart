4733df6910cf4a0938a05c23d15a710891835ef7
Nana-Manuel <emmanuelagbosu29@gmail.com>
2023-09-08 00:14:42 +0000

    ....

--- 4. Loops.dart
+++ 4. Loops.dart
@@ -1,19 +1,20 @@
-//while loop
-var i = 0;
-while (i < 4)
-{
-  print("This is a while loop");
-  i++;
-}
+void main() {
+  //while loop
+  var i = 0;
+  while (i < 4) {
+    print("This is a while loop");
+    i++;
+  }
 
 // Do While Loop
-int z = 0;
-do {
-  print("This is a do while loop");
-  i++;
-} while(z < 4);
+  int z = 0;
+  do {
+    print("This is a do while loop");
+    z++;
+  } while (z < 4);
 
 //For Loop
-for (var x = 0; x < 10; x++){
-  print("This is a for loop");
+  for (var x = 0; x < 10; x++) {
+    print("This is a for loop");
+  }
 }
