0e85871b20f414d2ab552371a74bb2c190344316
Nana-Manuel <emmanuelagbosu29@gmail.com>
2023-09-13 12:38:39 +0000

    Added the function file

---
+++ 5. Functions.dart
@@ -0,0 +1,22 @@
+// return_type funtname(required parameters){
+//   body;
+//   return value;
+// }
+
+import 'dart:ffi';
+
+void main() {
+  perimeter(12, 2);
+  print(Area(12, 2));
+}
+
+//void return type
+void perimeter(double lenght, double breadth) {
+  double Per = 2 * lenght + 2 * breadth;
+  print(Per);
+}
+
+//int return type
+int Area(int length, int breadth) {
+  return length * breadth;
+}
