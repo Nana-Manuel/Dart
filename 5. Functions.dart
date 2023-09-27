01466f7d4d96b778b53a711bc2e6ac00ff313eee
Nana-Manuel <emmanuelagbosu29@gmail.com>
2023-09-27 13:52:13 +0000

    Added functional expressions

--- 5. Functions.dart
+++ 5. Functions.dart
@@ -6,6 +6,7 @@
 void main() {
   perimeter(12, 2);
   print(Area(12, 2));
+  Area1(12,2);
 }
 
 //void return type
@@ -18,3 +19,8 @@ void perimeter(double lenght, double breadth) {
 int Area(int length, int breadth) {
   return length * breadth;
 }
+
+
+//functional expressions
+// We can convert one line funtions into expressions
+void Area1(int length, int breadth) => print("Area is ${length * breadth}");
