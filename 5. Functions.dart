bdfee8d601a32f14a635d41573a0c8428d6dd58a
Nana-Manuel <emmanuelagbosu29@gmail.com>
2023-10-10 14:35:18 +0000

    Added the named parameter block to the functions

--- 5. Functions.dart
+++ 5. Functions.dart
@@ -3,10 +3,15 @@
 //   return value;
 // }
 
+import 'dart:svg';
+
+import '2. Data Types and Varriables.dart';
+
 void main() {
   perimeter(12, 2);
   print(Area(12, 2));
-  Area1(12,2);
+  Area1(12, 2);
+  findVolume(10, height: 20, breadth: 2); //named parameters
 }
 
 //void return type
@@ -20,7 +25,17 @@ int Area(int length, int breadth) {
   return length * breadth;
 }
 
-
 //functional expressions
 // We can convert one line funtions into expressions
 void Area1(int length, int breadth) => print("Area is ${length * breadth}");
+
+//Named Parameter
+// Dont know why the sintax ia not working but it seems to be right from tutorials
+void findVolume(int length, {int breadth, int height})
+ {
+  print("Length: $length");
+  print("Breadth: $breadth");
+  print("Height: $height");
+
+  print(length * breadth * height);
+}
