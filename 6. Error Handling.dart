3d36064e6f28109d937d9440f5baef270499fd38
Nana-Manuel <emmanuelagbosu29@gmail.com>
2023-10-12 13:41:38 +0000

    added the finally clause and the catch clause

--- 6. Error Handling.dart
+++ 6. Error Handling.dart
@@ -5,23 +5,44 @@
 // 4. FINALLY CLAUSE
 
 void main() {
- 
-print("CASE 1");
-//CASE 1 is used when you are sure about the error 
+  print("CASE 1");
+//CASE 1 is used when you are sure about the error
   try {
     int result = 12 ~/ 0;
     print(result);
-  } on UnsupportedError { 
+  } on UnsupportedError {
     print("Can not divide by zero");
   }
 
-
-print("\nCASE 2");
+  print("\nCASE 2");
 // CASE 2 is used when the error is not known before hand
   try {
     int result = 12 ~/ 0;
     print(result);
-  } catch(e) {  
-    print("$e");
+  } catch (e) {
+    print("The error thrown is $e");
+  }
+
+  print("\nCASE 4");
+// CASE 4 is used when the error is not known before hand and the stack trace is also required
+// the stacke trace shows the line number where the error occured
+  try {
+    int result = 12 ~/ 0;
+    print(result);
+  } catch (e, s) {
+    print("The error thrown is $e");
+    print("STACK TRACE \n $s");
+  }
+
+  print("\nCASE 5");
+// CASE 5 is used when the error is not known before hand and the finally clause is also required
+// Finally clause is always executed irrespective of the error
+  try {
+    int result = 12 ~/ 0;
+    print(result);
+  } catch (e) {
+    print("The error thrown is $e");
+  } finally {
+    print("This is finally clause and is always executed");
   }
 }
