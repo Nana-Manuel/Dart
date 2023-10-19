9ecea610795dcc705e56092b112011b8d0349802
Nana-Manuel <emmanuelagbosu29@gmail.com>
2023-10-19 13:58:13 +0000

    ....

--- 7. OOP.dart
+++ 7. OOP.dart
@@ -1,9 +1,15 @@
+import 'dart:ffi';
+
 void main() {
   var firstStudent = Student();
   firstStudent.id = 3021220;
   firstStudent.name = "Emmanuel";
   firstStudent.sleep();
   firstStudent.study();
+
+  print("\n\n");
+  var employee1 = employee();
+  employee1.showdata();
 }
 
 class Student {
@@ -22,5 +28,29 @@ class Student {
 
   void study() {
     print("${this.name} is studying");
+  }
+}
+
+class employee {
+  int id = 0000000;
+  String name = "No Name";
+  var gender = 'M';
+  String address = "No Address";
+  String phone = "No Phone";
+  String email = "No Email";
+  String department = "No Department";
+
+  void showdata() {
+    print("Name: ${this.name}");
+    print("id Number: ${this.id}");
+    print("Gender: ${this.gender}");
+    print("Address: ${this.address}");
+    print("Phone: ${this.phone}");
+    print("Email: ${this.email}");
+    print("Department: ${this.department}");
+  }
+
+  void collectdata() {
+    
   }
 }
